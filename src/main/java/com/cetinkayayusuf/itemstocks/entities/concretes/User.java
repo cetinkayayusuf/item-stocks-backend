31f97133b83725b4ea8c7b2174dc00705196bdc8
cetinkayayusuf <39012641+cetinkayayusuf@users.noreply.github.com>
2023-01-06 11:24:58 +0300

    Add User Class

---
+++ src/main/java/com/cetinkayayusuf/itemstocks/entities/concretes/User.java
@@ -0,0 +1,48 @@
+package com.cetinkayayusuf.itemstocks.entities.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")})
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private long id;
+
+    @NotBlank
+    @Size(max = 30)
+    @Column(name = "username")
+    private String userName;
+
+    @NotBlank
+    @Email
+    @Size(max = 50)
+    @Column(name = "email")
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    @Column(name = "password")
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(	name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+}
