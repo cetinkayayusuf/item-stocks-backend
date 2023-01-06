d3f23da0545f426e6aad4767c6c0157e0cc6e15f
cetinkayayusuf <39012641+cetinkayayusuf@users.noreply.github.com>
2023-01-06 10:32:04 +0300

    Add required files

---
+++ src/main/java/com/cetinkayayusuf/itemstocks/ItemStocksApplication.java
@@ -0,0 +1,13 @@
+package com.cetinkayayusuf.itemstocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemStocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemStocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cetinkayayusuf/itemstocks/ItemStocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cetinkayayusuf.itemstocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemStocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
